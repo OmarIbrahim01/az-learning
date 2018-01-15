16544686192b82d494edd1ac095d565387095dd9
Omar Ibrahim <omar.ibrahim01@live.com>
2018-01-15 06:55:04 +0200

    Added Template

---
+++ resources/views/courses/course.blade.php
@@ -0,0 +1,125 @@
+@extends('layouts.main')
+
+@section('css')
+<link href="http://vjs.zencdn.net/6.6.0/video-js.css" rel="stylesheet">
+@stop
+
+@section('breadcrumbs')
+<div id="heading-breadcrumbs">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-7">
+                <h1>Category full</h1>
+            </div>
+            <div class="col-md-5">
+                <ul class="breadcrumb">
+                    <li><a href="index.html">Home</a>
+                    </li>
+                    <li>Category full</li>
+                </ul>
+
+            </div>
+        </div>
+    </div>
+</div>
+@stop
+
+@section('content')
+<div id="content">
+    <div class="container" style="padding-bottom: 115px;">
+
+        <div class="row">
+            <div class="col-md-12">
+                <div class="heading">
+                    <h2>Course Name - Trailer</h2>
+                </div>
+            </div>
+        </div>
+        <div class="row portfolio-project">
+
+            <section>
+
+                <div class="col-sm-8">
+                    <video id="my-video" class="video-js vjs-big-play-centered" controls preload="auto" height="500" style="margin: 0 auto; width: 100%;" 
+                      poster="MY_VIDEO_POSTER.jpg" data-setup="{}" >
+                        <source src="/storage/videos/trailer.MP4" type='video/mp4'>
+                        <source src="MY_VIDEO.webm" type='video/webm'>
+                        <p class="vjs-no-js">
+                          To view this video please enable JavaScript, and consider upgrading to a web browser that
+                          <a href="http://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a>
+                        </p>
+                    </video>
+                    <section>
+
+                        
+
+                            <div class="heading">
+                                <h3>About this Course</h3>
+                                <button type="button" class="btn btn-lg btn-template-main" style="float: right; margin-top: 20px;">Start Course</button>
+                            </div>
+
+                            <p>Bringing unlocked me an striking ye perceive. Mr by wound hours oh happy. Me in resolution pianoforte continuing we. Most my no spot felt by no. He he in forfeited furniture sweetness he arranging. Me tedious so to behaved
+                                written account ferrars moments. Too objection for elsewhere her preferred allowance her. Marianne shutters mr steepest to me. Up mr ignorant produced distance although is sociable blessing. Ham whom call all lain like.</p>
+
+                            <p>To sorry world an at do spoil along. Incommode he depending do frankness remainder to. Edward day almost active him friend thirty piqued. People as period twenty my extent as. Set was better abroad ham plenty secure had horses.
+                                Admiration has sir decisively excellence say everything inhabiting acceptance. Sooner settle add put you sudden him.</p>
+                        
+                    </section>
+                </div>
+
+                <div class="col-sm-4">
+                    <!-- *** MENUS AND FILTERS ***
+_________________________________________________________ -->
+                <div class="panel panel-default sidebar-menu">
+
+                    <div class="panel-heading">
+                        <h3 class="panel-title" style="padding-right: 50%;">Videos</h3>
+                    </div>
+
+                    <div class="panel-body">
+                        <ul class="nav nav-pills nav-stacked category-menu">
+                            <li>
+                                <a href="shop-category.html"><i class="fa fa-play" aria-hidden="true"></i> Video Name - Course Name</a>
+                                <a href="shop-category.html"><i class="fa fa-play" aria-hidden="true"></i> Video Name - Course Name</a>
+                                <a href="shop-category.html"><i class="fa fa-play" aria-hidden="true"></i> Video Name - Course Name</a>
+                                <a href="shop-category.html"><i class="fa fa-play" aria-hidden="true"></i> Video Name - Course Name</a>
+                                <a href="shop-category.html"><i class="fa fa-play" aria-hidden="true"></i> Video Name - Course Name</a>
+                                <a href="shop-category.html"><i class="fa fa-play" aria-hidden="true"></i> Video Name - Course Name</a>
+                                <a href="shop-category.html"><i class="fa fa-play" aria-hidden="true"></i> Video Name - Course Name</a>
+                                <a href="shop-category.html"><i class="fa fa-play" aria-hidden="true"></i> Video Name - Course Name</a>
+                                <a href="shop-category.html"><i class="fa fa-play" aria-hidden="true"></i> Video Name - Course Name</a>
+                                <a href="shop-category.html"><i class="fa fa-play" aria-hidden="true"></i> Video Name - Course Name</a>
+                                <a href="shop-category.html"><i class="fa fa-play" aria-hidden="true"></i> Video Name - Course Name</a>
+                                <a href="shop-category.html"><i class="fa fa-play" aria-hidden="true"></i> Video Name - Course Name</a>
+                                <a href="shop-category.html"><i class="fa fa-play" aria-hidden="true"></i> Video Name - Course Name</a>
+                                <a href="shop-category.html"><i class="fa fa-play" aria-hidden="true"></i> Video Name - Course Name</a>
+                                <a href="shop-category.html"><i class="fa fa-play" aria-hidden="true"></i> Video Name - Course Name</a>
+                                <a href="shop-category.html"><i class="fa fa-play" aria-hidden="true"></i> Video Name - Course Name</a>
+                                <a href="shop-category.html"><i class="fa fa-play" aria-hidden="true"></i> Video Name - Course Name</a>
+                                <a href="shop-category.html"><i class="fa fa-play" aria-hidden="true"></i> Video Name - Course Name</a>
+                                <a href="shop-category.html"><i class="fa fa-play" aria-hidden="true"></i> Video Name - Course Name</a>
+
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+                <!-- *** MENUS AND FILTERS END *** -->
+                </div>
+
+            </section>
+
+            
+
+        </div>
+
+        
+
+    </div>
+    <!-- /.container -->
+
+
+</div>
+<!-- /#content -->
+
+@stop
---
+++ resources/views/courses/library.blade.php
@@ -0,0 +1,216 @@
+@extends('layouts.main')
+
+@section('breadcrumbs')
+<div id="heading-breadcrumbs">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-7">
+                <h1>Category full</h1>
+            </div>
+            <div class="col-md-5">
+                <ul class="breadcrumb">
+                    <li><a href="index.html">Home</a>
+                    </li>
+                    <li>Category full</li>
+                </ul>
+
+            </div>
+        </div>
+    </div>
+</div>
+@stop
+
+@section('content')
+<div id="content">
+    <div class="container">
+        <h3>Library</h3>
+        <p class="text-muted lead text-left">These are all the Courses we offer.</p>
+
+        <div class="row products">
+            <div class="col-sm-10">
+
+                <div class="col-md-3 col-sm-4">
+                    <div class="product">
+                        <div class="image">
+                            <a href="shop-detail.html">
+                                <img src="img/product1.jpg" alt="" class="img-responsive image1">
+                            </a>
+                        </div>
+                        <!-- /.image -->
+                        <div class="text text-left">
+                            <h3 style="height: 30px; margin-bottom: 0; */"><a href="shop-detail.html">Fur coat</a></h3>
+                            <p>Course Details course details course detailscourse details</p>
+                        </div>
+                        <!-- /.text -->
+                    </div>
+                    <!-- /.product -->
+                </div>
+                <!-- /.col-md-4 -->
+
+                <div class="col-md-3 col-sm-4">
+                    <div class="product">
+                        <div class="image">
+                            <a href="shop-detail.html">
+                                <img src="img/product1.jpg" alt="" class="img-responsive image1">
+                            </a>
+                        </div>
+                        <!-- /.image -->
+                        <div class="text text-left">
+                            <h3 style="height: 30px; margin-bottom: 0; */"><a href="shop-detail.html">Fur coat</a></h3>
+                            <p>Course Details course details course detailscourse details</p>
+                        </div>
+                        <!-- /.text -->
+                    </div>
+                    <!-- /.product -->
+                </div>
+                <!-- /.col-md-4 -->
+
+                <div class="col-md-3 col-sm-4">
+                    <div class="product">
+                        <div class="image">
+                            <a href="shop-detail.html">
+                                <img src="img/product1.jpg" alt="" class="img-responsive image1">
+                            </a>
+                        </div>
+                        <!-- /.image -->
+                        <div class="text text-left">
+                            <h3 style="height: 30px; margin-bottom: 0; */"><a href="shop-detail.html">Fur coat</a></h3>
+                            <p>Course Details course details course detailscourse details</p>
+                        </div>
+                        <!-- /.text -->
+                    </div>
+                    <!-- /.product -->
+                </div>
+                <!-- /.col-md-4 -->
+
+                <div class="col-md-3 col-sm-4">
+                    <div class="product">
+                        <div class="image">
+                            <a href="shop-detail.html">
+                                <img src="img/product1.jpg" alt="" class="img-responsive image1">
+                            </a>
+                        </div>
+                        <!-- /.image -->
+                        <div class="text text-left">
+                            <h3 style="height: 30px; margin-bottom: 0; */"><a href="shop-detail.html">Fur coat</a></h3>
+                            <p>Course Details course details course detailscourse details</p>
+                        </div>
+                        <!-- /.text -->
+                    </div>
+                    <!-- /.product -->
+                </div>
+                <!-- /.col-md-4 -->
+
+                <div class="col-md-3 col-sm-4">
+                    <div class="product">
+                        <div class="image">
+                            <a href="shop-detail.html">
+                                <img src="img/product1.jpg" alt="" class="img-responsive image1">
+                            </a>
+                        </div>
+                        <!-- /.image -->
+                        <div class="text text-left">
+                            <h3 style="height: 30px; margin-bottom: 0; */"><a href="shop-detail.html">Fur coat</a></h3>
+                            <p>Course Details course details course detailscourse details</p>
+                        </div>
+                        <!-- /.text -->
+                    </div>
+                    <!-- /.product -->
+                </div>
+                <!-- /.col-md-4 -->
+
+                <div class="col-md-3 col-sm-4">
+                    <div class="product">
+                        <div class="image">
+                            <a href="shop-detail.html">
+                                <img src="img/product1.jpg" alt="" class="img-responsive image1">
+                            </a>
+                        </div>
+                        <!-- /.image -->
+                        <div class="text text-left">
+                            <h3 style="height: 30px; margin-bottom: 0; */"><a href="shop-detail.html">Fur coat</a></h3>
+                            <p>Course Details course details course detailscourse details</p>
+                        </div>
+                        <!-- /.text -->
+                    </div>
+                    <!-- /.product -->
+                </div>
+                <!-- /.col-md-4 -->
+
+                <div class="col-md-3 col-sm-4">
+                    <div class="product">
+                        <div class="image">
+                            <a href="shop-detail.html">
+                                <img src="img/product1.jpg" alt="" class="img-responsive image1">
+                            </a>
+                        </div>
+                        <!-- /.image -->
+                        <div class="text text-left">
+                            <h3 style="height: 30px; margin-bottom: 0; */"><a href="shop-detail.html">Fur coat</a></h3>
+                            <p>Course Details course details course detailscourse details</p>
+                        </div>
+                        <!-- /.text -->
+                    </div>
+                    <!-- /.product -->
+                </div>
+                <!-- /.col-md-4 -->
+
+                <div class="col-md-3 col-sm-4">
+                    <div class="product">
+                        <div class="image">
+                            <a href="shop-detail.html">
+                                <img src="img/product1.jpg" alt="" class="img-responsive image1">
+                            </a>
+                        </div>
+                        <!-- /.image -->
+                        <div class="text text-left">
+                            <h3 style="height: 30px; margin-bottom: 0; */"><a href="shop-detail.html">Fur coat</a></h3>
+                            <p>Course Details course details course detailscourse details</p>
+                        </div>
+                        <!-- /.text -->
+                    </div>
+                    <!-- /.product -->
+                </div>
+                <!-- /.col-md-4 -->
+            </div>
+
+            <div class="col-sm-2">
+
+                <!-- *** MENUS AND FILTERS ***
+_________________________________________________________ -->
+                <div class="panel panel-default sidebar-menu">
+
+                    <div class="panel-heading">
+                        <h3 class="panel-title" style="padding-right: 110px;">Categories</h3>
+                    </div>
+
+                    <div class="panel-body">
+                        <ul class="nav nav-pills nav-stacked category-menu">
+                            <li>
+                                <a href="shop-category.html">All Courses</a>
+                                <ul>
+                                    <li><a href="shop-category.html">T-shirts</a>
+                                    </li>
+                                    <li><a href="shop-category.html">Shirts</a>
+                                    </li>
+                                    <li><a href="shop-category.html">Pants</a>
+                                    </li>
+                                    <li><a href="shop-category.html">Accessories</a>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+                <!-- *** MENUS AND FILTERS END *** -->
+
+            </div>
+            <!-- /.col-md-3 -->
+
+        </div>
+        <!-- /.container -->
+    </div>
+    <!-- /#content -->
+</div>
+@stop
